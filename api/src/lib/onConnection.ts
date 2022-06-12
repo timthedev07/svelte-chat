b4b68c78e69bcbb98bbb6665080bb8b15c8ab77a
Tim <harvard90873@gmail.com>
2022-06-12 22:55:18 +0200

    update

--- api/src/lib/onConnection.ts
+++ api/src/lib/onConnection.ts
@@ -1,23 +1,51 @@
 import { Socket, Server } from "socket.io";
+import { getTimestampStr } from "../helper/getTimestampStr";
+import { loadMessages } from "../mongodb/functions/loadMessages";
 import { newMessage } from "../mongodb/functions/newMessage";
 import { ChatMessage } from "../mongodb/models/ChatMessage";
+import { WithMessageType } from "../types";
 
 const handler = (socket: Socket, ioServer: Server) => {
   console.log("new client");
 
-  // new message handler
-  socket.on("message", (message: string) => {
-    const messageObj = {
-      senderAuthId: "abcdefg12345",
-      message,
-      timeStamp: new Date().toLocaleString(),
-      roomId: "qwerty",
-    } as ChatMessage;
+  // join room handler
+  socket.on(
+    "joinRoom",
+    async ({ userId, roomId }: { [key: string]: string }) => {
+      // join the room
+      socket.join(roomId);
 
-    ioServer.emit("message", messageObj);
+      const existingMessages = await loadMessages(roomId);
 
-    newMessage(messageObj);
-  });
+      // populate existing messages
+      socket.emit("loadMessages", existingMessages);
+
+      // notice message
+      socket.broadcast.to(roomId).emit("message", {
+        message: `user<${userId}> has joined the room`,
+        roomId: roomId,
+        senderAuthId: "~",
+        timeStamp: getTimestampStr(),
+        type: "notice",
+      } as WithMessageType<ChatMessage>);
+
+      socket.on("message", (message: string) => {
+        const messageObj = {
+          senderAuthId: "abcdefg12345",
+          message,
+          timeStamp: getTimestampStr(),
+          roomId: "public",
+        } as ChatMessage;
+
+        ioServer.to(roomId).emit("message", {
+          ...messageObj,
+          type: "message",
+        });
+
+        newMessage(messageObj);
+      });
+    }
+  );
 };
 
 export const onConnection = (ioServer: Server) => {

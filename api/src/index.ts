9dc6db582000a172550de9a85f822f16b4b0d9f0
Tim <harvard90873@gmail.com>
2022-06-12 20:12:02 +0200

    more coding

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,16 +1,34 @@
 import express from "express";
 import { createServer } from "http";
 import { Server } from "socket.io";
+import { config } from "dotenv";
+
+config();
 
 const app = express();
 const http = createServer(app);
 const PORT = 8000;
-const ioServer = new Server(http);
+const ioServer = new Server(http, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
 
 http.listen(PORT, () => {
   console.log(`listening on http://localhost:${PORT}`);
 });
 
-ioServer.on("connection", (_socket) => {
+ioServer.on("connection", (socket) => {
   console.log("new client");
+
+  // new message handler
+  socket.on("message", (message: string) => {
+    const messageObj = {
+      from: "Jack",
+      message,
+      time: new Date().toLocaleString(),
+    };
+    ioServer.emit("message", messageObj);
+  });
 });
--- web/src/routes/index.svelte
+++ web/src/routes/index.svelte
@@ -1,5 +1,48 @@
-<h1 class="text-cyan-400">Welome to SvelteKit</h1>
-<p>
-  Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
-</p>
-d
+<script lang="ts">
+  import { io } from "../lib/socket";
+  import { onMount } from "svelte";
+  import type { Message } from "../types";
+
+  let messages: Message[] = [];
+  let draft: string;
+
+  onMount(() => {
+    io.on("message", (message) => {
+      // Listen to the message event
+      messages = [...messages, message];
+    });
+    io.on("name", (name) => {
+      // Another listener for the name:
+      // username = name // Update the name so it can be displayed
+    });
+  });
+
+  const sendMessage = () => {
+    const message = draft.trim();
+    if (!message) return;
+
+    draft = "";
+    io.emit("message", message); // Send the message
+  };
+</script>
+
+<ul>
+  {#each messages as message}
+    <li>
+      {message.message}
+    </li>
+  {/each}
+</ul>
+<form
+  action="#"
+  on:submit|preventDefault={sendMessage}
+  class="rounded-md border p-8"
+>
+  <input
+    bind:value={draft}
+    class="bg-slate-200/60 rounded-md px-5 py-2 border-slate-500/60"
+  />
+  <button class="px-5 py-3 rounded-md bg-black text-white" type="submit"
+    >Send</button
+  >
+</form>
